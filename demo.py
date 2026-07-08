7c462ab3be8b2f85ae537ac7c2415725ed519bb4
scc <1044155865@qq.com>
2026-07-08 10:45:45 +0800

    kjy

--- demo.py
+++ demo.py
@@ -1,8 +1,4 @@
 import secretflow as sf
 print("hello")
-<<<<<<< HEAD
-print("hello2")
-=======
-print("hello2")
-print("hello3")
->>>>>>> parent of 0eac8a2 (fix: errorTest)
+print("scc")
+print("kjy")
